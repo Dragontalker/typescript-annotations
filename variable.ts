cabc253beb0afe748468695d8a5e8bca65a7aa41
Richard <richard.yang.tong@gmail.com>
2021-04-16 12:18:27 -0400

    Added a new example.

--- variable.ts
+++ variable.ts
@@ -31,7 +31,15 @@ const logNumber: (i: number) => void = (i: number) => {
 // When to use annotations
 // 1) Function that returns the 'any' type
 const json = '{"x":10, "y":20}';
-const coordinates: { x:number; y: number } = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates); // {x: 10, y:20};
 
+// 2) When we decalre a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord;
 
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
