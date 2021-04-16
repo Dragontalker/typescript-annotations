008d069ae2b6878ab56d4dd8b575c89f5300a2ec
Richard <richard.yang.tong@gmail.com>
2021-04-16 14:25:47 -0400

    Added new code to inspect error in IDE.

--- variable.ts
+++ variable.ts
@@ -43,3 +43,15 @@ for (let i = 0; i < words.length; i++) {
     foundWord = true;
   }
 }
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero = false;
+
+for (let i = 0; i <numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i]
+  }
+}
+
+
